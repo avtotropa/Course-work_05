a37870f7c281a13348f31dbde69491f07690038d
autotropa <autotropa@yandex.ru>
2023-06-26 22:52:47 +0300

    Update

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-from utils import create_database, save_data_to_database
-from config import config
+from src.classes import DBManager
+from src.database import create_db, execute_sql_script, insert_data
+from src.employers import get_data_employers, get_vacancies
+from src.utils import config
 
 
 def main():
-    employer_ids = [3529, 1740, 1057, 2180, 84585, 78638, 4023, 15478, 39305, 3809]  # список айдишек работодателей
-    db_name = 'employers_and_vacancies'
+    url = 'https://api.hh.ru/employers'
+    area = '113'  # регион
+    db_name = 'head_hunter_db'
+    script_file = 'src/queries.sql'
     params = config()
 
-    create_database(db_name, params)
-    save_data_to_database(db_name, params, employer_ids)
+    employers = get_data_employers(url, area)  # получение списка компаний
+    vacancies = get_vacancies(employers)  # получение списка вакансий по компаниям
+
+    create_db(params, db_name)  # создание базы данных
+    execute_sql_script(params, db_name, script_file)  # создание таблиц в базе данных
+    insert_data(params, db_name, employers, vacancies)  # добавление данных в таблицы
+
+    db = DBManager(params, db_name)
+
+    #  Методы для работы с БД
+    db.get_companies_and_vacancies_count()  # Показывает все компании и количество вакансий у каждой компании
+    # db.get_all_vacancies()  # Показывает все вакансии (назв комп, назв вак, ЗП, ссылки на вакансию)
+    # db.get_avg_salary()  # Показывает среднюю зарплату по вакансиям
+    # db.get_vacancies_with_higher_salary()  # Показывает все вакансии, у которых зарплата выше средней
+
+    # keyword = 'Менеджер'
+    # db.get_vacancies_with_keyword(keyword)  # Показывает все вакансии, в названии которого содержится переданное слово
+
 
 if __name__ == '__main__':
     main()
-
---
+++ src/employers.py
@@ -0,0 +1,44 @@
+import requests
+
+
+def get_data_employers(url, area):
+    """Функция возвращает список работодателей у которых более десяти открытых вакансий"""
+    employers = []
+
+    for page in range(10):
+        params = {'area': area, 'page': page, 'per_page': 100}
+        response = requests.get(url, params).json()
+
+        for employer in response['items']:
+            if employer['open_vacancies'] > 10:
+                employers.append({
+                    'company_id': employer['id'],
+                    'company_name': employer['name'],
+                    'company_url': employer['alternate_url'],
+                    'company_open_vacancies': employer['open_vacancies'],
+                    'vacancies_url': employer['vacancies_url']
+                })
+    return employers
+
+
+def get_vacancies(data: list):
+    """Функция возвращает список данных о вакансиях по отдельному работодателю"""
+    vacancies = []
+    for employer in data:
+        response = requests.get(employer['vacancies_url']).json()
+        for company in response['items']:
+            if company['salary'] is not None:
+                vacancies.append({
+                    'vacancy_id': company['id'],
+                    'company_id': company['employer']['id'],
+                    'vacancy_name': company['name'],
+                    'city': company['area']['name'],
+                    'salary_from': company['salary']['from'],
+                    'salary_to': company['salary']['to'],
+                    'salary_currency': company['salary']['currency'],
+                    'vacancy_url': company['alternate_url']
+                })
+    return vacancies
+
+
+
